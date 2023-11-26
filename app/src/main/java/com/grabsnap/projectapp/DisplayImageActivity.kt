e9587a5c77feacd4fecc09985f5864cbada2a5dd
nazirxz <nzryanzr@gmail.com>
2023-11-26 12:07:35 +0700

    kualtias sudah bagus dan bisa crop (ada bug)

--- app/src/main/java/com/grabsnap/projectapp/DisplayImageActivity.kt
+++ app/src/main/java/com/grabsnap/projectapp/DisplayImageActivity.kt
@@ -10,12 +10,16 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.grabsnap.projectapp.databinding.ActivityImageBinding
 import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.CancellationSignal
 import android.os.ParcelFileDescriptor
 import android.print.*
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
 import androidx.core.content.FileProvider
 import java.text.SimpleDateFormat
@@ -28,21 +32,20 @@ import java.io.*
 
 class DisplayImageActivity : AppCompatActivity() {
 
+    private val REQUEST_CAMERA_PERMISSION = 1
+    private val CAMERA_REQUEST_CODE = 2
     private lateinit var binding: ActivityImageBinding
     private val WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 3
     private lateinit var photoBitmap: Bitmap
     private val UCROP_REQUEST_CODE = 123
+    private var photoFile: File? = null
+    private var currentPhotoPath: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityImageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Inisialisasi photoBitmap
-        photoBitmap = intent.getParcelableExtra("photo") ?: throw IllegalArgumentException("Photo bitmap is null")
-
-        // Gunakan photoBitmap setelah diinisialisasi
-        binding.displayImageView.setImageBitmap(photoBitmap)
 
         binding.printButton.setOnClickListener {
             if (photoBitmap != null) {
@@ -51,7 +54,11 @@ class DisplayImageActivity : AppCompatActivity() {
         }
 
         binding.displayImageView.setOnClickListener {
-            openUCropActivity()
+            if (hasCameraPermission()) {
+                openCamera()
+            } else {
+                requestCameraPermission()
+            }
         }
         binding.backIcon.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
@@ -60,13 +67,17 @@ class DisplayImageActivity : AppCompatActivity() {
         }
 
         binding.saveImageButton.setOnClickListener {
-            if (photoBitmap != null) {
+            if (::photoBitmap.isInitialized) {
                 if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                     saveImageToExternalStorage(photoBitmap)
                 } else {
                     // Request WRITE_EXTERNAL_STORAGE permission
                     ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), WRITE_EXTERNAL_STORAGE_REQUEST_CODE)
                 }
+            } else {
+                // Lakukan sesuatu jika photoBitmap belum diinisialisasi
+                // Contoh: Tampilkan pesan kesalahan
+                showToast("Photo has not been initialized")
             }
         }
 
@@ -76,33 +87,11 @@ class DisplayImageActivity : AppCompatActivity() {
             finish()
         }
     }
-    private fun openUCropActivity() {
-        val destinationUri = Uri.fromFile(File(cacheDir, "croppedImage"))
-        val options = UCrop.Options()
-        options.withAspectRatio(1f, 1f) // Sesuaikan rasio jika diperlukan
-
-        UCrop.of(getImageUri(this, photoBitmap), destinationUri)
-            .withOptions(options)
-            .start(this, UCROP_REQUEST_CODE)
+    private fun displayImage(bitmap: Bitmap) {
+        photoBitmap = bitmap
+        // Tampilkan foto yang sudah diambil ke ImageView atau lakukan operasi lain yang diperlukan
+        binding.displayImageView.setImageBitmap(photoBitmap)
     }
-    // Fungsi getImageUri untuk mendapatkan Uri dari bitmap
-    private fun getImageUri(inContext: Context, inImage: Bitmap): Uri {
-        val imagesDir = File(inContext.cacheDir, "images")
-        imagesDir.mkdirs()
-
-        val file = File(imagesDir, "image_${System.currentTimeMillis()}.png")
-        try {
-            val stream: OutputStream = FileOutputStream(file)
-            inImage.compress(Bitmap.CompressFormat.PNG, 100, stream)
-            stream.flush()
-            stream.close()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
-        return FileProvider.getUriForFile(inContext, "${inContext.packageName}.fileprovider", file)
-    }
-
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
@@ -118,22 +107,87 @@ class DisplayImageActivity : AppCompatActivity() {
             }
         }
     }
+    private fun hasCameraPermission(): Boolean {
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
+    }
+
+    private fun openCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                photoFile = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.grabsnap.projectapp.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE)
+                }
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun openUCropActivity(imageUri: Uri) {
+        val destinationUri = Uri.fromFile(createTempFile())
+        val options = UCrop.Options()
+        options.withAspectRatio(1f, 1f) // Sesuaikan rasio jika diperlukan
+        options.setCompressionQuality(80)
+        UCrop.of(imageUri, destinationUri)
+            .withOptions(options)
+            .start(this, UCROP_REQUEST_CODE)
+    }
 
     private fun saveImageToExternalStorage(image: Bitmap) {
-        val storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-        val timeStamp = System.currentTimeMillis()
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
         val fileName = "IMG_$timeStamp.png"
 
-        try {
-            if (!storageDir.exists()) {
-                storageDir.mkdirs()
-            }
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+            put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+            put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+        }
 
-            val imageFile = File(storageDir, fileName)
-            val imageOutputStream = FileOutputStream(imageFile)
-            image.compress(Bitmap.CompressFormat.PNG, 100, imageOutputStream)
-            imageOutputStream.close()
-            showToast("Image saved successfully in external storage as $fileName")
+        val resolver = contentResolver
+        var imageUri: Uri? = null
+        try {
+            val contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            val uri = resolver.insert(contentUri, contentValues)
+            if (uri != null) {
+                val imageOutputStream = resolver.openOutputStream(uri)
+                image.compress(Bitmap.CompressFormat.PNG, 100, imageOutputStream)
+                imageOutputStream?.close()
+                showToast("Image saved successfully in external storage as $fileName")
+            } else {
+                showToast("Failed to save image")
+            }
         } catch (e: IOException) {
             e.printStackTrace()
             showToast("Failed to save image")
@@ -215,24 +269,70 @@ class DisplayImageActivity : AppCompatActivity() {
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
-    private fun handleUCropResult(uri: Uri) {
-        try {
-            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
-            binding.displayImageView.setImageBitmap(bitmap) // Menampilkan gambar hasil crop pada displayImageView
-            photoBitmap = bitmap // Mengupdate photoBitmap dengan gambar hasil crop
-        } catch (e: IOException) {
-            e.printStackTrace()
+    private fun convertToTrueColor(negativeImage: Bitmap?): Bitmap? {
+        if (negativeImage == null) {
+            return null
         }
+
+        val width = negativeImage.width
+        val height = negativeImage.height
+        val trueColorImage = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                val pixelColor = negativeImage.getPixel(x, y)
+                val red = 250 - android.graphics.Color.red(pixelColor) - 25
+                val green = 250 - android.graphics.Color.green(pixelColor) - 50
+                val blue = 250 - android.graphics.Color.blue(pixelColor) - 50
+
+//                // Tambahkan pencetakan nilai-nilai warna
+//                Log.d("Warna", "Red: $red, Green: $green, Blue: $blue")
+
+                trueColorImage.setPixel(x, y, android.graphics.Color.rgb(red, green, blue))
+            }
+        }
+
+        return trueColorImage
+    }
+    private fun createTempFile(): File {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val imageFileName = "JPEG_$timeStamp"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            imageFileName,
+            ".jpg",
+            storageDir
+        )
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == UCROP_REQUEST_CODE && resultCode == RESULT_OK) {
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val imageFile = photoFile
+            if (imageFile != null) {
+                val imageUri = Uri.fromFile(imageFile)
+                openUCropActivity(imageUri)
+            }
+        } else if (requestCode == UCROP_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             val resultUri = UCrop.getOutput(data!!)
-            resultUri?.let { handleUCropResult(it) }
+            if (resultUri != null) {
+                // Dekode hasil UCrop
+                val imageBitmap = BitmapFactory.decodeFile(resultUri.path)
+
+                // Kompresi gambar sebelum konversi ke warna sejati
+                val compressedBitmap = compressBitmap(imageBitmap, 80) // Atur kualitas kompresi di sini
+
+                val trueColorBitmap = convertToTrueColor(compressedBitmap)
+                if (trueColorBitmap != null) {
+                    displayImage(trueColorBitmap) // Tetapkan gambar ke photoBitmap
+                }
+            }
         }
     }
-
+    private fun compressBitmap(bitmap: Bitmap, quality: Int): Bitmap {
+        val stream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, quality, stream)
+        return BitmapFactory.decodeStream(ByteArrayInputStream(stream.toByteArray()))
+    }
 }
 
 object BitmapConverter {
--- app/src/main/java/com/grabsnap/projectapp/MainActivity.kt
+++ app/src/main/java/com/grabsnap/projectapp/MainActivity.kt
@@ -16,9 +16,7 @@ import java.io.FileOutputStream
 import java.io.IOException
 class MainActivity : AppCompatActivity() {
 
-    private val REQUEST_CAMERA_PERMISSION = 1
-    private val CAMERA_REQUEST_CODE = 2
-    private val WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 3
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,87 +25,72 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnCamera.setOnClickListener {
-            if (hasCameraPermission()) {
-                openCamera()
-            } else {
-                requestCameraPermission()
-            }
+//            if (hasCameraPermission()) {
+//                openCamera()
+//            } else {
+//                requestCameraPermission()
+//            }
+            val intent = Intent(this, DisplayImageActivity::class.java)
+            startActivity(intent)
         }
         binding.btnAbout.setOnClickListener {
             val intent = Intent(this, AboutActivity::class.java)
             startActivity(intent)
-            finish()
         }
         binding.btnSignOut.setOnClickListener {
             finish()
         }
     }
 
-    private fun hasCameraPermission(): Boolean {
-        return (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
-    }
+//    private fun hasCameraPermission(): Boolean {
+//        return (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+//    }
+//
+//    private fun requestCameraPermission() {
+//        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
+//    }
+//
+//    private fun openCamera() {
+//        val cameraIntent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+//        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
+//    }
 
-    private fun requestCameraPermission() {
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
-    }
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+//            val photo: Bitmap? = data?.extras?.get("data") as Bitmap?
+//            val trueColorPhoto = convertToTrueColor(photo)  // Ubah gambar menjadi true color
+//            val intent = Intent(this, DisplayImageActivity::class.java)
+//            intent.putExtra("photo", trueColorPhoto)  // Kirim gambar true color sebagai data ekstra
+//            startActivity(intent)
+//        }
+//    }
 
-    private fun openCamera() {
-        val cameraIntent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
-            val photo: Bitmap? = data?.extras?.get("data") as Bitmap?
-            val trueColorPhoto = convertToTrueColor(photo)  // Ubah gambar menjadi true color
-            val intent = Intent(this, DisplayImageActivity::class.java)
-            intent.putExtra("photo", trueColorPhoto)  // Kirim gambar true color sebagai data ekstra
-            startActivity(intent)
-        }
-    }
-
-    private fun saveImage(image: Bitmap) {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-            val imageFile = File(storageDir, "true_color_image.jpg")
-
-            try {
-                val imageOutputStream = FileOutputStream(imageFile)
-                image.compress(Bitmap.CompressFormat.JPEG, 100, imageOutputStream)
-                imageOutputStream.close()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        } else {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), WRITE_EXTERNAL_STORAGE_REQUEST_CODE)
-        }
-    }
-
-    private fun convertToTrueColor(negativeImage: Bitmap?): Bitmap? {
-        if (negativeImage == null) {
-            return null
-        }
-
-        val width = negativeImage.width
-        val height = negativeImage.height
-        val trueColorImage = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-
-        for (x in 0 until width) {
-            for (y in 0 until height) {
-                val pixelColor = negativeImage.getPixel(x, y)
-                val red = 250 - android.graphics.Color.red(pixelColor) - 25
-                val green = 250 - android.graphics.Color.green(pixelColor) - 50
-                val blue = 250 - android.graphics.Color.blue(pixelColor) - 50
-
-                // Tambahkan pencetakan nilai-nilai warna
-                Log.d("Warna", "Red: $red, Green: $green, Blue: $blue")
-
-                trueColorImage.setPixel(x, y, android.graphics.Color.rgb(red, green, blue))
-            }
-        }
-
-        return trueColorImage
-    }
+//
+//    private fun convertToTrueColor(negativeImage: Bitmap?): Bitmap? {
+//        if (negativeImage == null) {
+//            return null
+//        }
+//
+//        val width = negativeImage.width
+//        val height = negativeImage.height
+//        val trueColorImage = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+//
+//        for (x in 0 until width) {
+//            for (y in 0 until height) {
+//                val pixelColor = negativeImage.getPixel(x, y)
+//                val red = 250 - android.graphics.Color.red(pixelColor) - 25
+//                val green = 250 - android.graphics.Color.green(pixelColor) - 50
+//                val blue = 250 - android.graphics.Color.blue(pixelColor) - 50
+//
+//                // Tambahkan pencetakan nilai-nilai warna
+//                Log.d("Warna", "Red: $red, Green: $green, Blue: $blue")
+//
+//                trueColorImage.setPixel(x, y, android.graphics.Color.rgb(red, green, blue))
+//            }
+//        }
+//
+//        return trueColorImage
+//    }
 
 }
